6ce6c936f2f0311f162f4e1c7ed2c85f102af741
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2017-11-13 21:14:54 -0700

    add page for HSR

--- js/script.js
+++ js/script.js
@@ -9,13 +9,6 @@ function smoothlink(id){
   document.getElementById(id).scrollIntoView({ 
   behavior: 'smooth',block: 'start'
 });
-//*/
-/*
-  let target = document.getElementById(id);
-  var body = document.getElementsByTagName("BODY")[0];
-  body.animate({
-    scrollTop: target.offset().top
-}, 1000);
-  */
+
 
 }
