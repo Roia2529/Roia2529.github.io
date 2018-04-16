9fc9faa0c4b3e8c32752e562eae8a048ffe599a4
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2018-04-16 13:53:29 -0600

    add text input for getting new data

--- angularexample/public/js/script.js
+++ angularexample/public/js/script.js
@@ -1,14 +1,34 @@
 
-var jsondata;
 var app = angular.module('myApp', []);
 
-app.controller('customersCtrl', function($scope, $http) {
-    $http.get("http://jsonplaceholder.typicode.com/posts").then(function(response) {
-        //jsondata = response.data;
-        $scope.names = response.data;
-        $scope.orderByField = 'userId';
-  		$scope.reverseSort = false;
-    });
+app.controller('customersCtrl', function($scope, $http,$window) {
+	$scope.link = "http://jsonplaceholder.typicode.com/posts";
+
+    // $http.get($scope.link).then(function(response) {
+    //     $scope.names = response.data;
+    //     $scope.orderByField = 'userId';
+  		// $scope.reverseSort = false;
+    // });
+    
+
+    $scope.getData = function(){
+	    	$http.get($scope.link).then(function(response) {
+	    	console.log('getData trigger');	
+	        $scope.names = response.data;
+	        $scope.orderByField = 'userId';
+	  		$scope.reverseSort = false;
+	    });
+    }
+    $scope.getData();
+    
+    $scope.updateData = function(){
+    	if (typeof ($scope.Name) == "undefined" || $scope.Name == "") {
+                    $window.alert("Please enter valid JSON data link");
+                    return;
+        }
+        $scope.link = $scope.Name;
+        $scope.getData();
+    };
 });
 
 
