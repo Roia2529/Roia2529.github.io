014ef344a5e10e16eb67d646ed001908ae4fd701
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2018-04-16 15:03:02 -0600

    add icon to show ascending/decending

--- angularexample/public/js/script.js
+++ angularexample/public/js/script.js
@@ -4,23 +4,16 @@ var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http,$window) {
 	$scope.link = "http://jsonplaceholder.typicode.com/posts";
 
-    // $http.get($scope.link).then(function(response) {
-    //     $scope.names = response.data;
-    //     $scope.orderByField = 'userId';
-  		// $scope.reverseSort = false;
-    // });
-    
-
     $scope.getData = function(){
 	    	$http.get($scope.link).then(function(response) {
-	    	console.log('getData trigger');	
+	    	//console.log('getData trigger');	
 	        $scope.names = response.data;
 	        $scope.orderByField = 'userId';
 	  		$scope.reverseSort = false;
 	    });
     }
     $scope.getData();
-    
+
     $scope.updateData = function(){
     	if (typeof ($scope.Name) == "undefined" || $scope.Name == "") {
                     $window.alert("Please enter valid JSON data link");
