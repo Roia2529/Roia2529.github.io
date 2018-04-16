c3a6d2002f96e691a86bcfc75ad50618b392a5bf
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2018-04-16 15:13:36 -0600

    check link validation

--- angularexample/public/js/script.js
+++ angularexample/public/js/script.js
@@ -2,11 +2,15 @@
 var app = angular.module('myApp', []);
 
 app.controller('customersCtrl', function($scope, $http,$window) {
-	$scope.link = "http://jsonplaceholder.typicode.com/posts";
+	$scope.link = "https://jsonplaceholder.typicode.com/posts";
 
     $scope.getData = function(){
 	    	$http.get($scope.link).then(function(response) {
-	    	//console.log('getData trigger');	
+	    	//console.log('getData trigger');
+	    	if(typeof(response)=="undefined"){
+	    		$window.alert("Fail to get JSON data.");
+                    return;
+	    	}	
 	        $scope.names = response.data;
 	        $scope.orderByField = 'userId';
 	  		$scope.reverseSort = false;
@@ -16,8 +20,12 @@ app.controller('customersCtrl', function($scope, $http,$window) {
 
     $scope.updateData = function(){
     	if (typeof ($scope.Name) == "undefined" || $scope.Name == "") {
-                    $window.alert("Please enter valid JSON data link");
-                    return;
+            $window.alert("Please enter valid JSON data link");
+            return;
+        }
+        else if($scope.Name.indexOf('https://') != 0){
+        	$window.alert("Please enter link starting with https");
+            return;
         }
         $scope.link = $scope.Name;
         $scope.getData();
