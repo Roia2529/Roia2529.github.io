36ea19509d0d22ca86b416e72775510fd02dcbb6
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2018-09-22 19:52:41 -0600

    add react component

--- symtest/public/js/script.js
+++ symtest/public/js/script.js
@@ -1,3 +1,14 @@
+function timedText() {
+    var x = document.getElementById("txt");
+    for(var i=0;i<10;i++){
+    	setTimeout(function(){
+        	console.log(i);
+        }, 1000);
+    }
+}
+
+timedText();
+
         let pieChart = new PieChart();
         let tChart = new TableChart(pieChart);
         
